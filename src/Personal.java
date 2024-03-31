3f3a717324314b589a6d7351ded2175f587dc2d1
Varaan785 <156232089+Varaan785@users.noreply.github.com>
2024-03-31 21:08:40 +0200

    Valerian31.03.24

---
+++ src/Personal.java
@@ -0,0 +1,11 @@
+public class Personal {
+    String name;
+    String department;
+    int age;
+
+    public Personal(String name, String department, int age) {
+        this.name = name;
+        this.department = department;
+        this.age = age;
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    int id;
+    String name;
+    float price;
+    String typeOfProduct;
+
+    public Product(int id, String name, float price, String typeOfProduct) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.typeOfProduct = typeOfProduct;
+    }
+}
