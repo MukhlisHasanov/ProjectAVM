023467a36630e217dfff2fdb65e36ab61a2804ae
Mukhlis Hasanov <muhlis.gasanov@gmail.com>
2024-04-01 01:48:45 +0400

    Mukhlis004
    Work with Valerian and Andrey

--- src/GanjlikMall.java
+++ src/GanjlikMall.java
@@ -3,25 +3,21 @@ import java.util.List;
 
 public class GanjlikMall {
     public static void main(String[] args) {
-        ClientDataBase clientDataBase = new ClientDataBase();
-        System.out.println(clientDataBase);
+        List<Client> clients = new ArrayList<>();
+        String[] names = {"Valerian", "Mukhlis", "Andrey", "Alexander", "Sergey"};
+        int[] ids = {00001, 00002, 00003, 00004, 00005};
+        int[] ages = {38, 30, 39, 41, 42};
 
-//        List<Client> clients = new ArrayList<>();
-//        String[] names = {"Valerian", "Mukhlis", "Andrey", "Alexander"};
-//        int[] ids = {00001, 00002, 00003, 00004};
-//        int[] ages = {38, 30, 39, 41};
-//
-//        for (int i = 0; i < names.length; i++) {
-//            clients.add(new Client(names[i], ids[i], ages[i]));
-//        }
-//
-//
-//        for (Client client : clients) {
-//            System.out.println("Client: " + client.getName() + ", ID: " + client.getId() + ", Age: " + client.getAge());
-//            // client.buy(laptop);
-//            // andrey.buy(cheese);
-//            // mukhlis.watch(movie);
-//            // clients.giveDiscount(15);
-//        }
+        for (int i = 0; i < names.length; i++) {
+            clients.add(new Client(names[i], ids[i], ages[i]));
+        }
+
+        for (Client client : clients) {
+            System.out.println("Client: " + client.getName() + ", ID: " + client.getId() + ", Age: " + client.getAge());
+            // client.buy(laptop);
+            // andrey.buy(cheese);
+            // mukhlis.watch(movie);
+            // clients.giveDiscount(15);
+        }
     }
 }
