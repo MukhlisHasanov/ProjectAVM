1052415664d0dc3bb903002514b67e14ddb9de67
Mukhlis Hasanov <muhlis.gasanov@gmail.com>
2024-03-31 23:07:29 +0400

    delete test classes

--- src/Andrey.java
+++
@@ -1,6 +0,0 @@
-public class Andrey {
-    public static void main(String[] args) {
-        System.out.println("Hello from Andrey!");
-    }
-
-}
--- src/Mukhlis.java
+++
@@ -1,5 +0,0 @@
-public class Mukhlis {
-    public static void main(String[] args) {
-        System.out.println("Test from Mukhlis");
-    }
-}
