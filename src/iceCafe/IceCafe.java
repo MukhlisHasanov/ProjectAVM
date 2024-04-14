31e862068a1d117248b56460e847e98fa2198874
Varaan785 <156232089+Varaan785@users.noreply.github.com>
2024-04-14 22:07:44 +0200

    Update IceCafe.java

--- src/iceCafe/IceCafe.java
+++ src/iceCafe/IceCafe.java
@@ -23,8 +23,8 @@ public class IceCafe {
         iceHouse.add(2,Ice2,"strawberry with chocolate",30);
         iceHouse.add(3,Cafe,"latte macchiato",30);
         System.out.println(iceHouse);
-        order.addBuyProduct(new IceProduct(1,Ice1, "Fantasy", 5));
-        order.addBuyProduct(new IceProduct(2,Ice2,"Fantasy",1));
+        order.addBuyProduct(new IceProduct(1,Ice1, "Ice", 5));
+        order.addBuyProduct(new IceProduct(2,Ice2,"Ice",1));
         order.addBuyProduct(new IceProduct(3,Cafe,"Cafe",1));
         System.out.println(order);
         iceHouse.delOfOrder(order.getShoppingList());
